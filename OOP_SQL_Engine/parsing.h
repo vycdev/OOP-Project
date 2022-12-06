5d063e839e6e9913dfe1fd981fd2eebc7d75344f
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-12-06 22:31:00 +0200

    Phase 1 final commit

--- OOP_SQL_Engine/parsing.h
+++ OOP_SQL_Engine/parsing.h
@@ -287,17 +287,21 @@ void lexer(string* tokens, int noOfTokens, int line) {
 		tokenizer(tokens[5], ",", noOfInputValues, inputValues);
 		
 			// check the paranthesis 
-	/*	if (tokens[5].length() >= 2) {
+		if (tokens[5].length() >= 2) {
 			int lastPos = inputValues[noOfInputValues - 1].length() - 1;
-			if (!(tableColumns[noOfColumns - 1][lastPos] == ')' && tableColumns[noOfColumns - 1][lastPos - 1])) {
-				string err = "Error: Invalid table formatting at line " + to_string(line) + ". Expected \"))\" but instead got \"" + tableColumns[noOfColumns - 1][lastPos] + tableColumns[noOfColumns - 1][lastPos - 1] + "\".";
+			if (!(tokens[5][lastPos] == ')' && tokens[5][0]) == '(') {
+				string err = "Error: Invalid table formatting at line " + to_string(line) + ". Expected \"()\" but instead got \"" + tokens[5][0] + tokens[5][lastPos]+ "\".";
 				throw err;
 			}
-		} */
+		} 
 
 			// check the number of tokens
+		// Call the db methods to find the number of columns in the table with the name tokens[2]
+		checkNoOfTokensMin(noOfInputValues, ..., line);
+		checkNoOfTokensMax(noOfInputValues, ..., line);
 
 			// check the validity of the tokens according to their respective columns in the database
+
 
 			// do the insertion
 
@@ -308,19 +312,51 @@ void lexer(string* tokens, int noOfTokens, int line) {
 		// SELECT (id,nume,grupa)  FROM  studenti
 		// SELECT ALL FROM studenti WHERE id = 1
 		// SELECT ALL FROM studenti
-
+		
+		checkNoOfTokensMin(noOfTokens, 4, line);
+		
 
 		return;
 	}
 	if (tokens[0] == "DELETE") {
 		// DELETE FROM studenti WHERE nume = “John”
+		// |
+		// |
+		// V
+		// DELETE FROM studenti WHERE nume=John
+		checkNoOfTokensMin(noOfTokens, 5, line);
+		checkNoOfTokensMax(noOfTokens, 5, line);
 
+		checkToken(tokens[1], "FROM", line);
+
+		checkValidName(tokens[2], line);
+
+		checkToken(tokens[3], "WHERE", line);
+
+		int noOfCompValues = 0;
+		string* compValues = new string[2];
+
+		tokenizer(tokens[4], "=", noOfCompValues, compValues);
+
+		checkNoOfTokensMin(noOfCompValues, 2, line);
+		checkNoOfTokensMax(noOfCompValues, 2, line);
+		
+		checkValidName(compValues[0], line);
+		// check the type of the column and verify if the second value is comparable 
+		
+		// call the methods
 
 		return;
 	}
 	if (tokens[0] == "UPDATE") {
 		// UPDATE studenti SET nume = “Alice” WHERE id = 1
 		// UPDATE studenti SET nume = “Alice” WHERE nume= “Bianca”
+		// |
+		// |
+		// V
+		// UPDATE studenti SET nume=Alice WHERE nume=Bianca
+
+
 
 		return;
 	}
